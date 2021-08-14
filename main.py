1683831d531dfb0abd10d699339668a464a34499
anvinder <anvinder_singh26@yahoo.co.in>
2021-08-14 15:28:45 -0700

    Update main.py
    adding code for password protecting the pdf.

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-
 import qrcode
+import PyPDF2
+from MyQR import myqr as mq
 
-input_data = "my name is xyz"
-#Creating an instance of qrcode
+pdfFile = open('qrco.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(pdfFile)
+pdfWriter = PyPDF2.PdfFileWriter()
+for pageNum in range(pdfReader.numPages):
+    pdfWriter.addPage(pdfReader.getPage(pageNum))
+
+pdfWriter.encrypt('password')
+resultPdf = open('encrypted_output.pdf', 'wb')
+pdfWriter.write(resultPdf)
+resultPdf.close()
+
+#input_data = resultPdf
+input_data = 'asd.jpg'
 qr = qrcode.QRCode(
-        version=1,     # defines dimensions of the qr code (LxB)
+        version=5,                                              # defines dimensions of the qr code (LxB)
         error_correction=qrcode.constants.ERROR_CORRECT_H,      # About 30% or fewer errors can be corrected.
-        box_size=5,     # defines dimensions of the box that contains QRcode.
+        box_size=5,                                             # defines dimensions of the box that contains QRcode
         border=5)
 qr.add_data(input_data)
 qr.make(fit=True)
-img = qr.make_image(fill='black', back_color='transparent')
-img.save('qrcode001.png')
-
+img = qr.make_image(fill='black', back_color='white')
+img.save('qrcode.png')
