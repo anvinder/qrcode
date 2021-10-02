438c0929f016b2fd5f538db572f3d2a74a51da79
anvinder <anvinder_singh26@yahoo.co.in>
2021-10-02 12:51:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-import qrcode
+import os
+from os import listdir
+from os.path import isfile, join
 import PyPDF2
-from MyQR import myqr as mq
 
-pdfFile = open('qrco.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFile)
-pdfWriter = PyPDF2.PdfFileWriter()
-for pageNum in range(pdfReader.numPages):
-    pdfWriter.addPage(pdfReader.getPage(pageNum))
+class qrcode:
+    def __init__(self):
+        self.mypath = os.getcwd()
+        self.file_list = []
+        self.pdf_file_list = []
+    def File_list(self):
+        for f in listdir(self.mypath):
+            if isfile(join(self.mypath,f)):
+                self.file_list = self.file_list + [f]
+        self.encrypt_file(self.file_list)
 
-pdfWriter.encrypt('password')
-resultPdf = open('encrypted_output.pdf', 'wb')
-pdfWriter.write(resultPdf)
-resultPdf.close()
+    def encrypt_file(self, file_list):
+        for item in self.file_list:
+            if item.endswith('.pdf') and 'encrypt' not in item:
+                self.pdf_file_list += [item]
+        print(self.pdf_file_list)
+        for item in self.pdf_file_list:
+            pdfFile = open(item, 'rb')
+            pdfReader = PyPDF2.PdfFileReader(pdfFile)
+            pdfWriter = PyPDF2.PdfFileWriter()
+            for pageNum in range(pdfReader.numPages):
+                pdfWriter.addPage(pdfReader.getPage(pageNum))
+            pdfWriter.encrypt('password')
+            resultPdf = open(item + '_encrypt.pdf', 'wb')
+            pdfWriter.write(resultPdf)
+            resultPdf.close()
 
-#input_data = resultPdf
-input_data = 'asd.jpg'
-qr = qrcode.QRCode(
-        version=5,                                              # defines dimensions of the qr code (LxB)
-        error_correction=qrcode.constants.ERROR_CORRECT_H,      # About 30% or fewer errors can be corrected.
-        box_size=5,                                             # defines dimensions of the box that contains QRcode
-        border=5)
-qr.add_data(input_data)
-qr.make(fit=True)
-img = qr.make_image(fill='black', back_color='white')
-img.save('qrcode.png')
+
+if __name__ == "__main__":
+    object_qrcode = qrcode()
+    object_qrcode.File_list()
